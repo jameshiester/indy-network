cd3b1d6ce137b056bed7bfc1a311314641968ac6
James Hiester <james.hiester@fake.com>
2025-08-08 14:04:31 -0400

    updating sg rules

--- terraform/infrastructure/main.tf
+++ terraform/infrastructure/main.tf
@@ -77,7 +77,7 @@ resource "aws_security_group" "ec2_security_group" {
 
   egress {
     from_port   = 9704
-    to_port     = 9702
+    to_port     = 9704
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
