73b6c2986d82219d0c1869309d86dc64b85fccf2
James Hiester <james.hiester@fake.com>
2025-08-10 11:45:04 -0400

    bump

--- terraform/infrastructure/modules/ec2/user-data.sh
+++ terraform/infrastructure/modules/ec2/user-data.sh
@@ -28,6 +28,7 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 # sudo yum update -y
 sudo yum install -q -y amazon-cloudwatch-agent yum-utils systemd-networkd unzip
 echo "installing docker..."
+
 # sudo amazon-linux-extras install -y docker
 # sudo systemctl enable --now docker
 # sudo usermod -a -G docker ec2-user
