b6fb8b9dd280044b2e822800c89720ca2c31ae6a
James Hiester <james.hiester@fake.com>
2025-08-18 12:45:22 -0400

    updating variables

--- terraform/infrastructure/modules/ecs/variables.tf
+++ terraform/infrastructure/modules/ecs/variables.tf
@@ -54,16 +54,6 @@ variable "private_subnets" {
   type        = list(string)
 }
 
-variable "EnvCode" {
-  description = "2 character code used to name all resources e.g. 'pd' for production"
-  type        = string
-}
-
-variable "EnvTag" {
-  description = "Environment tag value. All resources are created with an 'Environment' tag name and the value you set here"
-  type        = string
-}
-
 variable "server_ecr_repo" {
   description = "Name of Amazon ECR repository for indy server"
   type        = string
--- terraform/infrastructure/output.tf
+++ terraform/infrastructure/output.tf
@@ -14,7 +14,7 @@ output "compose_bucket_name" {
   value       = aws_s3_bucket.compose_bucket.bucket
 }
 
-output "compose_bucket_name" {
-  description = "Name of the S3 bucket containing compose files"
-  value       = aws_s3_bucket.compose_bucket.bucket
+output "genesis_bucket_name" {
+  description = "Name of the S3 bucket containing genesis files"
+  value       = aws_s3_bucket.genesis_bucket.bucket
 }
