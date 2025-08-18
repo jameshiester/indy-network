92183056db2350b478d35c0f9f8143ef060b2d55
James Hiester <james.hiester@fake.com>
2025-08-18 16:25:08 -0400

    changing port to a string

--- terraform/infrastructure/modules/ecs/main.tf
+++ terraform/infrastructure/modules/ecs/main.tf
@@ -55,7 +55,7 @@ resource "aws_ecs_task_definition" "mswebapp" {
         },
         {
           name  = "DB_PORT"
-          value = var.db_port
+          value = "${var.db_port}"
         },
         {
           name  = "DB_HOST"
