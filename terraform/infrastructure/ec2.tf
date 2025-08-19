f4bd611ba38576631428b07653840f3480023199
James Hiester <james.hiester@fake.com>
2025-08-19 10:44:17 -0400

    updating build config

--- terraform/infrastructure/ec2.tf
+++ terraform/infrastructure/ec2.tf
@@ -61,7 +61,7 @@ data "aws_iam_policy_document" "instance_policy" {
   statement {
     effect    = "Allow"
     actions   = ["s3:ListBucket"]
-    resources = ["${aws_s3_bucket.genesis_bucket.arn}/*"]
+    resources = ["${aws_s3_bucket.compose_bucket.arn}/*"]
   }
   statement {
     effect    = "Allow"
