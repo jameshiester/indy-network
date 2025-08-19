1ced7cb95135112fd4e9864e0e21ad32cbf85e6d
James Hiester <james.hiester@fake.com>
2025-08-19 11:46:39 -0400

    updating permissions for ec2

--- terraform/infrastructure/ec2.tf
+++ terraform/infrastructure/ec2.tf
@@ -61,7 +61,12 @@ data "aws_iam_policy_document" "instance_policy" {
   statement {
     effect    = "Allow"
     actions   = ["s3:ListBucket"]
-    resources = ["${aws_s3_bucket.compose_bucket.arn}/*"]
+    resources = ["${aws_s3_bucket.compose_bucket.arn}"]
+  }
+  statement {
+    effect    = "Allow"
+    actions   = ["s3:ListBucket"]
+    resources = ["*"]
   }
   statement {
     effect    = "Allow"
