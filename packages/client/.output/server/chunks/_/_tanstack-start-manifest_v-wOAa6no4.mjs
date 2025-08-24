8ec46703b69136b8dea3ca7030345e1f5aec130b
James Hiester <james.hiester@fake.com>
2025-08-24 14:07:18 -0400

    deleting unused files

--- packages/client/.output/server/chunks/_/_tanstack-start-manifest_v-wOAa6no4.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ "routes": { "__root__": { "filePath": "C:/code/indy-network/packages/client/src/routes/__root.tsx", "children": ["/"], "preloads": ["/assets/main-Bgcx4UA1.js"], "assets": [] }, "/": { "filePath": "C:/code/indy-network/packages/client/src/routes/index.tsx", "assets": [], "preloads": ["/assets/index-DiNe9cmD.js"] } }, "clientEntry": "/assets/main-Bgcx4UA1.js" });
-
-export { tsrStartManifest };
-//# sourceMappingURL=_tanstack-start-manifest_v-wOAa6no4.mjs.map
