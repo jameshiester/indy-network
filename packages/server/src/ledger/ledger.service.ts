3764aa7db39a6e715c3bab74ae6a76339008e220
James Hiester <james.hiester@fake.com>
2025-08-24 18:26:54 -0400

    updating

--- packages/server/src/ledger/ledger.service.ts
+++ packages/server/src/ledger/ledger.service.ts
@@ -127,7 +127,7 @@ export class LedgerService {
     try {
       const response = await this.fetchTransactionFromMonitor(ledger, 1);
       this.logger.log(
-        `${ledger} transactions: ${Object.values(response)[0]}, ${Object.keys(response)}`,
+        `transaction 1 from ledger ${ledger}: ${(response as any).data}}`,
       );
     } catch (error) {
       this.logger.error(
